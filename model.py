680655597e2ea4306bb8e269fb8198e664fd9077
zhengnan77 <138441156+zhengnan77@users.noreply.github.com>
2023-07-06 16:24:17 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.cross_decomposition import PLSRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import cross_val_score, GridSearchCV, LeaveOneOut
